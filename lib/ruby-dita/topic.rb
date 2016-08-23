4bb5dfa7f5de5c76cd72c61dcf82a6e46eca5961
ludocracy <bansuki@gmail.com>
2016-08-23 17:39:32 -0500

    had to change #topic to put args directly under <topic>, not <body>

--- lib/ruby-dita/topic.rb
+++ lib/ruby-dita/topic.rb
@@ -4,13 +4,13 @@ module Dita
   include Duxml
 
   # @param title [String, Element] string or XML rich-text
-  # @param content [Array] optional topic body content in array form
+  # @param content [Array] optional topic content in array form; added as direct children of <topic>, not <body>
   # @return [Element] valid Dita <topic>
   def topic(title, content=nil)
     t = Element.new('topic')
     t[:id] = "topic#{t.object_id.to_s}"
     t << Element.new('title', [title])
-    t << Element.new('body', content) if content
+    t << content if content
     t
   end
 end
--- test/ruby-dita/topic_test.rb
+++ test/ruby-dita/topic_test.rb
@@ -17,9 +17,9 @@ class TopicTest < Test::Unit::TestCase
   end
 
   def test_topic
-    x = topic('ruby-dita', %w(some <p>content</p>))
+    x = topic('ruby-dita', [Element.new('body', ['some <p>content</p>'])])
     assert_match /<topic id="topic[0-9]{8}"\/>/, x.sclone.to_s
     assert_equal '<title>ruby-dita</title>', x.title.to_s
-    assert_equal '<body>some<p>content</p></body>', x.body.to_s
+    assert_equal '<body>some <p>content</p></body>', x.body.to_s
   end
 end
